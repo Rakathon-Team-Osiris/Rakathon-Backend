53baeb70578c84b9f226bf458a03dc18bdb06e9a
Omkar Amlan Krishna <58391249+OmkarAmlan@users.noreply.github.com>
2024-08-30 18:24:45 +0530

    removed vectordb.py

--- vectordb.py
+++ vectordb.py
@@ -1,17 +1,1 @@
-from pinecone import Pinecone
 
-index = pc.Index("slay-vector-db")
-
-array =[]
-
-ans = index.query(
-    vector=[0.1]*1024,
-    top_k=5,
-    include_values=True,
-    include_metadata=True,
-)
-
-for i in range(len(ans['matches'])):
-    array.append(ans['matches'][i]['id'])
-
-print(array)
